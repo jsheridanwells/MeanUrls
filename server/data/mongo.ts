b39ef62eca8324379855c31844a8be5800d3569b
jsheridanwells <jsheridanwells@gmail.com>
2021-02-02 12:14:59 -0600

    mongo db class and updated dev scripts

---
+++ server/data/mongo.ts
@@ -0,0 +1,42 @@
+import * as mongo from 'mongodb';
+
+export interface MongoDbConfig {
+  appUser: string;
+  appPassword: string;
+  dbName: string;
+  dbCollectionName: string;
+  hostName: string;
+  mongoPort: string | number;
+}
+
+export class Mongo {
+  public static client: mongo.MongoClient;
+
+  public static buildMongoUrl(config: MongoDbConfig): string {
+    return 'mongodb://'
+      + `${ config.appUser }:${ encodeURIComponent(config.appPassword) }`
+      + `@${ config.hostName }:${ config.mongoPort }`
+      + `/${ config.dbName }`;
+  }
+
+
+  public static connect(url: string): Promise<any> {
+    return new Promise<any>((res, rej) => {
+      mongo.MongoClient.connect(url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      }, (err
+        : any, client: mongo.MongoClient) => {
+        if (err) rej(err);
+        else {
+          Mongo.client = client;
+          res(client);
+        }
+      });
+    })
+  }
+
+  public static disconnect(): void {
+    Mongo.client.close();
+  }
+}
